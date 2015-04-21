a0f307d6197348ef4dfa66ddb65a9c2486921c1d
Ella Haines <ellatrue@yahoo.com>
2015-04-20 22:04:52 -0600

    Added documentation folder and milestone-1 document with outline. Still need to fix configuration settings.

---
+++ documentation/milestone-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Personal Website Project</title>
+	</head>
+	<body>
+		<h1>Purpose</h1>
+		<p>I would like this site to help establish me as a web developer by showcasing my skills.</p>
+		<h1>Audience</h1>
+		<p>
+			My intended audience for this site consists of potential employers and clients, and secondarily
+			other developers.
+		</p>
+		<h1>Goal</h1>
+		<p>
+			My goal for this site is for it to serve as something like a portfolio for my work. I'd like to showcase
+			my skills in PHP, HTML, JavaScript, Java and Python.
+			I want to use it to achieve full-time employment or to attract potential freelance customers.
+		</p>
+	</body>
+</html>

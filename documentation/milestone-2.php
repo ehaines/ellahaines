b8012160d6a0adca42debecaaa0d1253f47b91fd
Ella Haines <haines.ella@gmail.com>
2015-05-03 21:57:43 -0600

    Completed Larry the Developer Persona, added Use Case

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -8,6 +8,20 @@
 	<body>
 		<h1>Personal Website Project: Milestone 2</h1>
 		<h2>Persona: Larry the Developer</h2>
+		<ul>
+			<li><strong>Job: </strong>Heads the web development team at a mid-sized tech company.</li>
+			<li><strong>Technology: </strong>Desktop computer at the office.</li>
+			<li><strong>Behaviors and Attitudes: </strong>Larry is very results-oriented and more interested in the
+				quality of someone's work than whether they have a degree in the field.</li>
+			<li><strong>Frustrations and Needs: </strong>Larry is frustrated by not finding enough developers focused on
+				making responsive mobile design. For this reason he may view the website on his mobile phone.</li>
+		</ul>
+		<h2>Use Case</h2>
+		<p><strong>Scenario: </strong>Larry has received my business card or resume with a link to my website,
+			and is visiting the site to see what I can do. He is primarily interested in the gallery of my recent projects.
+			He is evaluating me as candidate for a job and deciding whether to call for an interview.
+			Larry only has a short time to evaluate my website, because he has to evaluate 5 other candidates.
+		</p>
 	</body>
 
 </html>

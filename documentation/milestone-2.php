8f56687a6b0df6ab9de2b417bb2d56f82915d968
Ella Haines <haines.ella@gmail.com>
2015-05-03 21:50:31 -0600

    Began Personal Website Project Milestone 2 - basic html doc & started writing Larry the Developer persona

---
+++ documentation/milestone-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Personal Website Project: Milestone 2</title>
+	</head>
+	<body>
+		<h1>Personal Website Project: Milestone 2</h1>
+		<h2>Persona: Larry the Developer</h2>
+	</body>
+
+</html>

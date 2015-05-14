2ac999c7bead2259d836cd1e5562e2eea219ee29
Ella Haines <haines.ella@gmail.com>
2015-05-13 23:17:31 -0600

    started milestone 3 and uploaded wireframes

---
+++ documentation/milestone-3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Personal Website Project: Milestone 3</title>
+	</head>
+	<body>
+		<h1>Milestone 3: Wireframes</h1>
+
+		<h2>Wireframe 1: Desktop, About/Gallery/Contact</h2>
+		<img src="../img/desktopmain.png.png" alt="A wireframe for the main page on desktop">
+
+		<h2>Wireframe 2: Mobile, About/Gallery/Contact</h2>
+		<img src="../img/mobile.png" alt="A wireframe of the mobile page">
+	</body>
+</html>

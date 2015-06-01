0fac3b24bf8c18e7550e60aa9b47e42ce7263c61
Ella Haines <haines.ella@gmail.com>
2015-05-31 22:22:01 -0600

    Changed the look of the games page and added a background image. Made games.css the only css file used for this page.

--- games.php
+++ games.php
@@ -18,7 +18,6 @@
 		<link rel="stylesheet" href="src/css/swipebox.css">
 
 		<!--Custom CSS Stylesheets-->
-		<link rel="stylesheet" href="/css/main.css"/>
 		<link rel="stylesheet" href="/css/games.css"/>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -34,7 +33,6 @@
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
 
-		<link rel="stylesheet" href="/css/main.css"/>
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<!--swipebox plugin-->
